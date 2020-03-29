3802d3aa571af1e194fa7ad05bfb45d221c9e2b3
sankamille <luc.brulet@epitech.eu>
2020-03-29 21:41:27 +1100

    [~] add comment into service.go

--- pkg/user/service.go
+++ pkg/user/service.go
@@ -2,38 +2,42 @@ package user
 
 import "github.com/lbrulet/Go-api-test/pkg/models"
 
+//Service is a structure to store the user repository
 type Service struct {
 	r Repository
 }
 
+//NewService is the constructor of Service
 func NewService(r Repository) *Service {
 	return &Service{r: r}
 }
 
+//Migrate is a method to make change on the database
 func (s *Service) Migrate() {
 	s.r.Migrate()
 }
 
+//GetUserByID is a method to get a user by ID
 func (s *Service) GetUserByID(ID int) (*models.User, error) {
 	return s.r.GetUserByID(ID)
 }
 
+//DeleteUserByID is a method to delete a user by ID
 func (s *Service) DeleteUserByID(ID int) error {
 	return s.r.DeleteUserByID(ID)
 }
 
+//UpdateUserByID is a method to update a user by ID
 func (s *Service) UpdateUserByID(user *models.User) error {
 	return s.r.UpdateUserByID(user)
 }
 
+//InsertUser is a method to insert a user
 func (s *Service) InsertUser(user *models.User) error {
 	return s.r.InsertUser(user)
 }
 
+//GetAllUsers is a method to get all users
 func (s *Service) GetAllUsers() ([]*models.User, error) {
 	return s.r.GetAllUsers()
 }
-
-
-
-
